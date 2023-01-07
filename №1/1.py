e03f0599e066eda77e318ebc62c46a5d3851e088
Ivan_Kalinyuk <ivan_kalinyuk01@mail.ru>
2023-01-07 12:37:34 +0300

    Решение 1 задачи

---
+++ №1/1.py
@@ -0,0 +1,12 @@
+import random
+n = int(input("Введите количество элементов Вашего списка: "))
+ar = []
+for i in range(n):
+    ar.append(random.randint(0,20))
+print(f"Вот список, заполненный произвольными числами: {ar}")
+sum = 0
+i=1
+while i < n:
+    sum = sum + ar[i]
+    i = i + 2
+print(f"Сумма чисел, стоящих на нечетных позициях из этого списка = {sum}")
